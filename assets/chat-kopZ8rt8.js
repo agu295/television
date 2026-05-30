794eaf50641b45d12512c05ea5726408d8403124
agu295 <39743807+agu295@users.noreply.github.com>
2026-05-30 19:51:22 -0300

    Add files via upload

---
+++ assets/chat-kopZ8rt8.js
@@ -0,0 +1,6 @@
+import{c as n}from"./index-Doyal9nA.js";/**
+ * @license lucide-react v1.14.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]],r=n("send",c),o=["#E50914","#F5A623","#4CAF50","#2196F3","#9C27B0","#FF5722","#00BCD4","#E91E63","#3F51B5","#009688","#FF9800","#607D8B","#795548","#CDDC39","#673AB7"];function h(a){let t=0;for(let e=0;e<a.length;e++)t=(t<<5)-t+a.charCodeAt(e);return o[Math.abs(t)%o.length]}export{r as S,h as g};
